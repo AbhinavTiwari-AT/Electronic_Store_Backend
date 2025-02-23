60459142477ffc62656e1f68f1bce429d1a3d240
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-02-23 16:11:27 +0530

    added product and productDto classes

---
+++ src/main/java/com/abhinav/electronic/Dto/ProductDto.java
@@ -0,0 +1,39 @@
+package com.abhinav.electronic.Dto;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProductDto {
+	
+	
+	private String productId;
+	
+	
+	private String title;
+	
+	private String description;
+	
+	private int price;
+	
+	private int quantity;
+	
+	private Date addedDate;
+	
+	private boolean live;
+	
+	private boolean stock;
+	
+	private int discountedPrice;
+
+}
