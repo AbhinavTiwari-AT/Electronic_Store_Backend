1b4b51209f9e5b426d03862b139b6121e1ef46e0
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-02-24 17:22:13 +0530

    implimented mapping of category and product

--- src/main/java/com/abhinav/electronic/Entities/Category.java
+++ src/main/java/com/abhinav/electronic/Entities/Category.java
@@ -1,8 +1,15 @@
 package com.abhinav.electronic.Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,4 +39,11 @@ public class Category {
 	private String coverImage;
 	
 	//other attributes if you have....
+	
+	//mapping category to product
+	
+	@OneToMany(mappedBy ="category", cascade =  CascadeType.ALL,fetch = FetchType.LAZY)
+	private List<Product> products = new ArrayList<>();
+	
+	
 }
