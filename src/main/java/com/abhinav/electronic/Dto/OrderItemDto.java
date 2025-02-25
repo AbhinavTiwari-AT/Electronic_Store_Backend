ee872ac4b772f26a92bd9e38581f59a23a783136
Abhinav Tiwari <abhinavtiwari3056@gmail.com>
2025-02-26 00:07:59 +0530

    Implemented Order and OredeItems Class with its DTO class

---
+++ src/main/java/com/abhinav/electronic/Dto/OrderItemDto.java
@@ -0,0 +1,26 @@
+package com.abhinav.electronic.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class OrderItemDto {
+	
+	private int orderItemId;
+	
+	private int quantity;
+	
+	private int totalPrice;
+	
+	private ProductDto product;
+	
+	//private OrderDto order;
+
+}
---
+++ src/main/java/com/abhinav/electronic/Entities/Order.java
@@ -0,0 +1,56 @@
+package com.abhinav.electronic.Entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+	
+	@Id
+	private String orderId;
+	
+	//PENDING,DELIVERED,DISPATCHED
+	private String orderStatu;
+	
+	//NOTPAID,PAID
+	//enum
+	//boolean false=> Not paid || true => paid
+	private String paymentStatu;
+	
+	private int orderAmount;
+	
+	@Column(length = 1000)
+	private String billingAddress;
+	
+	private String billingPhone;
+	
+	private Date orderedDate;
+	
+	private Date deliveredDate;
+	
+	//user
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name  ="user_id" )
+	private User user;
+	
+	//
+	@OneToMany(mappedBy = "order",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	private List<OrderItem> orderItems = new ArrayList<>();
+}
+ 
